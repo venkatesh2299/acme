bf841e93a4303f30da4ed67deb2956e805ab8b31
venkatesh2299 <vkms2299@gmail.com>
2021-02-01 14:40:31 +0530

    Fixes #3: created all database migrations

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=acme_db',
     'username' => 'root',
-    'password' => '',
+    'password' => 'mysql2299$',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
